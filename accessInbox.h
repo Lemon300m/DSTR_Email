257195ec24efdae51492ab18539d403a74605608
Lemon300m <165396814+Lemon300m@users.noreply.github.com>
2024-11-01 19:46:56 +0800

    Add files via upload

---
+++ accessInbox.h
@@ -0,0 +1,10 @@
+#ifndef ACCESSINBOX_H
+#define ACCESSINBOX_H
+
+#include <iostream>
+
+inline void accessInbox() {
+    std::cout << "Inbox functionality not yet implemented.\n";
+}
+
+#endif
---
+++ performSearch.h
@@ -0,0 +1,10 @@
+#ifndef PERFORMSEARCH_H
+#define PERFORMSEARCH_H
+
+#include <iostream>
+
+inline void performSearch() {
+    std::cout << "Search functionality not yet implemented.\n";
+}
+
+#endif
